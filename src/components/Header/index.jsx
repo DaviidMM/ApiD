32e53e90178d3d3a6a73ab3e01a0ebc5507a37c2
DaviidMM <davicitoo1612@gmail.com>
2022-06-02 21:36:21 +0200

    Add new style to logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,13 +3,14 @@ import Logo from "../Icons/Logo";
 
 export default function Header() {
   return (
-    <div className="h-[100px] header flex flex-row justify-between items-center font-semibold bg-slate-900 w-full px-16 2xl:px-96 text-blue-400">
-      <div className="h-full">
-        <Link to="/" className="flex flex-row items-end h-full py-4">
-          <Logo className="w-12 h-12" />
-          <h1 className="text-5xl">pid</h1>
-        </Link>
-      </div>
+    <div className="h-[100px] header flex flex-row justify-between items-center font-semibold bg-slate-900 w-full py-4 px-16 2xl:px-96 text-blue-400">
+      <Link
+        to="/"
+        className="flex flex-row items-end h-full p-3 rounded-lg hover:bg-slate-800 hover:text-red-300"
+      >
+        <Logo className="w-11 h-11" />
+        <h1 className="hidden text-5xl lg:block">pid</h1>
+      </Link>
       <a
         href="https://github.com/DaviidMM"
         className="transition-all brightness-75 hover:brightness-150"
