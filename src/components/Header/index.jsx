d025097a693636c52e5ce51516d40e598d16e9b3
DaviidMM <davicitoo1612@gmail.com>
2022-06-02 19:14:44 +0200

    Add page logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,15 @@
 import { Link } from "wouter";
+import Logo from "../Icons/Logo";
 
 export default function Header() {
   return (
-    <div className="h-[100px] header flex flex-row justify-between items-center font-semibold bg-slate-900 w-full px-16 2xl:px-96">
-      <Link to="/">
-        <a className="hover:brightness-125">
-          <h1 className="text-5xl text-[#219ebc]">Apify</h1>
-        </a>
-      </Link>
+    <div className="h-[100px] header flex flex-row justify-between items-center font-semibold bg-slate-900 w-full px-16 2xl:px-96 text-blue-400">
+      <div className="h-full">
+        <Link to="/" className="flex flex-row items-end h-full py-4">
+          <Logo className="w-12 h-12" />
+          <h1 className="text-5xl">pify</h1>
+        </Link>
+      </div>
       <a
         href="https://github.com/DaviidMM"
         className="transition-all brightness-75 hover:brightness-150"
